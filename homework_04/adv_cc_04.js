ed117d4d19c3b789fdfb9c3d4ca0db084b045c3f
Sam <ss11976@nyu.edu>
2021-02-27 11:22:52 +0530

    hw_04

--- homework_04/adv_cc_04.js
+++ homework_04/adv_cc_04.js
@@ -1,58 +1,202 @@
-var numberofPoints = 20;
-var trianglegridSize;
-var triangleshape = [];
-var slider1;
-var slider2;
-var startStop = false;
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-  var myButton = select('#first');
-  myButton.mousePressed(startTriangles);
-  trianglegridSize = width/numberofPoints;
-	for (var i = 0; i < width; i += trianglegridSize){
-    for (var j = 0; j < height; j += trianglegridSize){
-      triangleshape.push(new Triangles(i, j))
-    }
-  }
-  createP("pixelify!")
-  slider1 = createSlider(0, 100, 0);
-  createP("adjust flash speed (at your own risk)")
-  slider2 = createSlider(-2, 2, 0);
+var evening; var fall; var morning; var night; var noon; var rain; var snow; var spring; var summer; var sun; var wind; var winter;
+var table;
+var headers = [];
+var dropdown;
+var images = [];
+var circles = [];
+var circleCounter = 0;
+
+function preload(){
+  evening = loadImage("evening.PNG");
+  fall = loadImage("fall.PNG");
+  morning = loadImage("morning.PNG");
+  night = loadImage("night.PNG");
+  noon = loadImage("noon.PNG");
+  rain = loadImage("rain.PNG");
+  snow = loadImage("snow.PNG");
+  spring = loadImage("spring.PNG");
+  summer = loadImage("summer.PNG");
+  sun = loadImage("sun.PNG");
+  wind = loadImage("wind.PNG");
+  winter = loadImage("winter.PNG");
+  table = loadTable('assets/responses.csv', 'csv', 'header');
 }
 
-function startTriangles() {
-  startStop =! startStop
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+	imageMode(CENTER);
+
+  headers = [table.columns[1],table.columns[2],table.columns[3],table.columns[4]];
+
+  dropdown = createSelect();
+  dropdown.position(10,10);
+  dropdown.option("which question?", 0)
+  dropdown.option(headers[0], 1);
+  dropdown.option(headers[1], 2);
+  dropdown.option(headers[2], 3);
+  dropdown.option(headers[3], 4);
+  for(var i = 0; i < table.getRowCount(); i++){
+    for(var j = 0; j < (table.getColumnCount() - 1); j++){
+      images.push(new makingImages(table.getString(i, j)));
+    }
+  }
+  for(var a = 0; a < table.getRowCount(); a++){
+    circles.push(new makingCircles(table.getNum(a, 4)));
+		circleCounter++;
+  }
 }
+
 function draw() {
   background(0);
-  noStroke();
-  for(var i = 0; i < triangleshape.length; i++){
-    if (startStop){
-      triangleshape[i].colorchange();
+  if(dropdown.value() == 1){
+    background(0);
+    for(var i = 0; i < images.length; i++){
+      images[i].whichSeason();
+			images[i].move();
     }
-    triangleshape[i].render();
+  } 
+  else if(dropdown.value() == 2){
+    background(0);
+    for(var b = 0; b < images.length; b++){
+      images[b].whichWeather();
+			images[b].move();
+    }
+  }
+  else if(dropdown.value() == 3){
+    background(0);
+    for(var c = 0; c < images.length; c++){
+      images[c].timeofDay();
+			images[c].move();
+    }
+  }
+  else if(dropdown.value() == 4){
+    background(0);
+    for(var d = 0; d < circles.length; d++){
+      circles[d].render();
+			circles[d].move();
+    }
   }
 }
-class Triangles{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-		this.trstartingCol = color(random(255), random(255), random(255));
-    this.trendingCol = color(random(255), random(255), random(255));
-    this.bgValue = random();
-    this.newCosValue = random(0.01, 0.9);
+
+class makingImages{
+  constructor(season){
+    this.season = season;
+    this.x = random(width);
+    this.y = 10;
+    this.speedY = random(0, 5);
   }
 
-  render() {
-    var backgroundColor = lerpColor(this.trstartingCol, this.trendingCol, cos(this.bgValue));
-    fill(backgroundColor);
-    triangle(this.x, this.y, (this.x + trianglegridSize + slider1.value()), this.y, this.x, (this.y + trianglegridSize + slider1.value()));
+  move(){
+    this.y += this.speedY;
+    if (this.y > windowHeight || this.y < 0){
+      this.speedY*= -1;
+    }
   }
 
-  colorchange(){
-    this.bgValue = this.bgValue + this.newCosValue + slider2.value();
+  timeofDay(){
+    var eveningcounter = 0; var morningcounter = 0; var nightcounter = 0; var nooncounter = 0;
+    if (this.season == 'evenings (dusk)') {
+      eveningcounter++
+      for (var i = 0; i <= eveningcounter; i++) {
+        image(evening, this.x, this.y, 150, 200);
+      }
+    }
+    else if(this.season == 'early morning (dawn)') {
+      morningcounter++
+      for (var k = 0; k <= morningcounter; k++) {
+        image(morning, this.x, this.y, 150, 200);
+      }
+    }
+    else if(this.season == 'late night') {
+      nightcounter++
+      for (var l = 0; l <= nightcounter; l++) {
+        image(night, this.x, this.y, 150, 200);
+      }
+    }
+    else{
+      nooncounter++
+      for (var m = 0; m <= nooncounter; m++) {
+        image(noon, this.x, this.y, 120, 120);
+      }
+    }
+  }
+
+  whichSeason(){
+    var fallcounter = 0; var springcounter = 0; var summercounter = 0; var wintercounter = 0;
+    if(this.season == 'fall') {
+      fallcounter++
+      for (var i = 0; i <= fallcounter; i++) {
+        image(fall, this.x, this.y, 150, 150);
+      }
+    }
+    else if(this.season == 'spring') {
+      springcounter++
+      for (var n = 0; n <= springcounter; n++) {
+        image(spring, this.x, this.y, 150, 200);
+      }
+    }
+    else if(this.season == 'summer') {
+      summercounter++
+      for (var o = 0; o <= summercounter; o++) {
+        image(summer, this.x, this.y, 150, 200);
+      }
+    }
+    else{
+      wintercounter++
+      for (var p = 0; p <= wintercounter; p++) {
+        image(winter, this.x, this.y, 150, 150);
+      }
+    }
+  }
+
+  whichWeather(){
+    var raincounter = 0; var snowcounter = 0; var suncounter = 0; var windcounter = 0;
+    if(this.season == 'raining') {
+      raincounter++
+      for (var i = 0; i <= raincounter; i++) {
+        image(rain, this.x, this.y, 150, 200);
+      }
+    }
+    else if(this.season == 'snowing') {
+      snowcounter++
+      for (var q = 0; q <= snowcounter; q++) {
+        image(snow, this.x, this.y, 150, 200);
+      }
+    }
+    else if(this.season == 'sunny') {
+      suncounter++
+      for (var r = 0; r <= suncounter; r++) {
+        image(sun, this.x, this.y, 150, 200);
+      }
+    }
+    else{
+      windcounter++
+      for (var s = 0; s <= windcounter; s++) {
+        image(wind, this.x, this.y, 150, 200);
+      }
+    }
   }
 }
 
-
-
+class makingCircles{
+  contructor(diameter){
+		this.x = random(width);
+    this.y = 10;
+    this.speedY = random(0, 5);
+    this.diameter = diameter;
+  }
+	move() {
+		this.y += this.speedY;
+		if (this.y > windowHeight || this.y < 0){
+		this.speedY *= -1;
+		}
+	}
+  render(){
+    strokeWeight(10);
+    stroke('white');
+    fill('white');
+		for (var i = 0; i <= circleCounter; i++) {
+			circle(this.x, this.y, (this.diameter + 1) * 10);
+		}
+  }
+}
