3e1c210d424a9eb6701574c9b4198ae6db5ca0c6
Sam <ss11976@nyu.edu>
2021-04-01 08:28:58 +0530

    local code

--- homework_07/server.js
+++ homework_07/server.js
@@ -1,9 +1,17 @@
 console.log("My server is running!");
 
 var express = require('express');
-
 var app = express();
-
 var server = app.listen(3000);
-
 app.use(express.static('public'));
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+	console.log("new connection!!" + socket.id);
+	socket.on('pattern', patternMsg);
+	function patternMsg(data) {
+		socket.broadcast.emit('pattern', data);
+	}
+}
